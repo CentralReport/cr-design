a189151083af48c2288f10adb56e3a252a551234
Charles-Emmanuel <che.camus@gmail.com>
2014-07-16 21:39:23 +0200

    Updated bootstrap-additions

--- dist/js/centralreport.js
+++ dist/js/centralreport.js
@@ -1,4 +1,4 @@
-/*! CentralReport_design - DEV - 2014-07-14 */
+/*! CentralReport_design - DEV - 2014-07-16 */
 if (function(global, factory) {
     "object" == typeof module && "object" == typeof module.exports ? module.exports = global.document ? factory(global, !0) : function(w) {
         if (!w.document) throw new Error("jQuery requires a window with a document");
